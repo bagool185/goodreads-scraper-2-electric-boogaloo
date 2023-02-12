b87db4128025dd6d3c7bab7eb23a66738cec0a4d
AKelly404 <harley901@hotmail.com>
2023-02-12 20:22:53 +0000

    Added getCurrentReading function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { By } = require('selenium-webdriver');
 const webdriver = require('selenium-webdriver');
 const firefox = require('selenium-webdriver/firefox');
 
@@ -9,10 +10,25 @@ let driver = new webdriver.Builder()
 const baseUrl = "http://goodreads.com";
 const userId = "82924012-bagool";
 
-driver.get(`${baseUrl}/user/show/${userId}`);
+driver.get(`${baseUrl}/review/list/${userId}?shelf=currently-reading`);
 
 const cleanDrivers = () => {
     driver.quit();
 }
+
+async function getCurrentReading (){
+    let titles = await driver.findElements(
+        By.className("title"))
+    // let title = titles[1];
+    // console.log(await title.findElement(By.css("a")).getAttribute("title"));
+    titles.splice(0, 1);
+    let names = [];
+    for (let title of titles){
+        names.push(await title.findElement(By.css("a")).getAttribute("title"));
+    }
+    return names;
+};
+
+getCurrentReading().then((anything) => {console.log(anything)});
 
 setTimeout(cleanDrivers, 5000);
