102f4098b2703fce7b3f9d38e9f20b6938b42cd8
bagool <ioan.adrian98@gmail.com>
2023-02-12 19:07:28 +0000

    Navigates to goodreads

--- src/index.js
+++ src/index.js
@@ -3,5 +3,8 @@ const firefox = require('selenium-webdriver/firefox');
 
 let driver = new webdriver.Builder()
     .forBrowser('firefox')
-    .setFirefoxOptions(/* ... */)
+    // .setFirefoxOptions(new firefox.Options().headless())
     .build();
+
+driver.get("http://goodreads.com");
+driver.quit();
