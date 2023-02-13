8e86db8c47a89605a84f9cf695f8eeb0685e1602
AKelly404 <harley901@hotmail.com>
2023-02-13 19:10:36 +0000

    Added search function

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ let driver = new webdriver.Builder()
 
 const baseUrl = "http://goodreads.com";
 const userId = "82924012-bagool";
+const searchData = "THE BIBLE";
 
-driver.get(`${baseUrl}/review/list/${userId}?shelf=currently-reading`);
 
 const cleanDrivers = () => {
     driver.quit();
 }
 
 async function getCurrentReading (){
+    driver.get(`${baseUrl}/review/list/${userId}?shelf=currently-reading`);
+
     let titles = await driver.findElements(By.className("title"))
     // console.log(await title.findElement(By.css("a")).getAttribute("title"));
     titles.splice(0, 1);
@@ -27,25 +29,40 @@ async function getCurrentReading (){
     return names;
 };
 
-// async function getTopRated () {
-//     driver.get(`${baseUrl}/review/list/${userId}?shelf=read&sort=rating`);
+async function getTopRated () {
+    driver.get(`${baseUrl}/review/list/${userId}?shelf=read&sort=rating`);
 
-//     let ratedTitles = await driver.findElements(By.className("title"))
-//     ratedTitles.splice(0,1);
-//     let ratedNames = [];
-//     for (let name of ratedTitles){
-//         ratedNames.push(await name.findElement(By.css("a")).getAttribute("title"));
-//     }
-//     ratedNames.length = 5;
-//     // console.log(ratedNames[0]);
-//     return ratedNames;
-// }
-getCurrentReading().then((anything) => {
-    console.log("You are currently reading: ");
-    console.log(anything)});
+    let ratedTitles = await driver.findElements(By.className("title"));
+    ratedTitles.splice(0,1);
+    let ratedNames = [];
+    for (let name of ratedTitles){
+        ratedNames.push(await name.findElement(By.css("a")).getAttribute("title"));
+    }
+    ratedNames.length = 5;
+    return ratedNames;
+}
+
+async function searchBook(){
+    driver.get(`${baseUrl}/search?q=${searchData}`);
+
+    let searchResults = await driver.findElements(By.className("bookTitle"));
+    let searchTitles = [];
+    for (let book of searchResults) {
+        searchTitles.push(await book.getText());
+    }
+    searchTitles.length = 5;
+    return searchTitles;
+}
+
+// searchBook().then( (searchTitles) => {
+// console.log(searchTitles)});
+
+// getCurrentReading().then((anything) => {
+//     console.log("You are currently reading: ");
+//     console.log(anything)});
 
 // getTopRated().then((anything2) => {
 //     console.log("Your top rated books are: ");
 //     console.log(anything2)});
 
-setTimeout(cleanDrivers, 20000);
+setTimeout(cleanDrivers, 10000);
