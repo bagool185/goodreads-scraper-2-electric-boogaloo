97fe529c3f690d485ef1cd7b108c18ae685129b2
AKelly404 <harley901@hotmail.com>
2023-03-15 21:37:32 +0000

    Added validation for user IDs/URLs

--- src/index.js
+++ src/index.js
@@ -12,14 +12,14 @@ const embedUtils = require('./embeds.js');
 
 //Open browser window, headless allows for it to run in the background
 
-    let driver = new webdriver.Builder()
-    .forBrowser('firefox')
-    // .setFirefoxOptions(new firefox.Options().headless())
-    .build();
+let driver = new webdriver.Builder()
+  .forBrowser('firefox')
+  // .setFirefoxOptions(new firefox.Options().headless())
+  .build();
 
 //Get data from goodreads 
 
-async function searchBook(searchData){
+async function searchBook(searchData) {
   driver.get(`${baseUrl}/search?q=${searchData}`);
 
   let searchResult = await driver.findElement(By.css("tr"));
@@ -27,82 +27,82 @@ async function searchBook(searchData){
   let title = await searchResult.findElement(By.className("bookTitle")).getText();
   let author = await searchResult.findElement(By.className("authorName")).getText();
   let cover = await searchResult.findElement(By.css("img")).getAttribute("src");
-  cover = cover.replace("._SY75_","");
+  cover = cover.replace("._SY75_", "");
   let url = await searchResult.findElement(By.className("bookTitle")).getAttribute("href");
-  searchBook.push({title, author, cover, url});
+  searchBook.push({ title, author, cover, url });
 
-return searchBook;
+  return searchBook;
 }
 
-async function getCurrentReading (userId){
-    driver.get(`${baseUrl}/review/list/${userId}?shelf=currently-reading`);
+async function getCurrentReading(userId) {
+  driver.get(`${baseUrl}/review/list/${userId}?shelf=currently-reading`);
 
-    let rows = await driver.findElements(By.className("bookalike"))
-    let books = [];
-    for (let row of rows){
-        let title = await row.findElement(By.className("title")).getText();
-        let author = await row.findElement(By.className("author")).getText();
-        let avgRating = await row.findElement(By.className("avg_rating")).getText();
-        let cover = await row.findElement(By.css("img")).getAttribute("src");
-        cover = cover.replace("._SY75_","");
-        let url = await row.findElement(By.css("a")).getAttribute("href");
-        books.push({title, author, avgRating, cover, url});
-    }
-    return books;
+  let rows = await driver.findElements(By.className("bookalike"))
+  let books = [];
+  for (let row of rows) {
+    let title = await row.findElement(By.className("title")).getText();
+    let author = await row.findElement(By.className("author")).getText();
+    let avgRating = await row.findElement(By.className("avg_rating")).getText();
+    let cover = await row.findElement(By.css("img")).getAttribute("src");
+    cover = cover.replace("._SY75_", "");
+    let url = await row.findElement(By.css("a")).getAttribute("href");
+    books.push({ title, author, avgRating, cover, url });
+  }
+  return books;
 }
 
-async function getTopRated (userID) {
-    driver.get(`${baseUrl}/review/list/${userID}?shelf=read&sort=rating`);
+async function getTopRated(userID) {
+  driver.get(`${baseUrl}/review/list/${userID}?shelf=read&sort=rating`);
 
-    let ratedRows = await driver.findElements(By.className("bookalike"));
-    ratedRows.length = 5;
-    let books = [];
-    for (let row of ratedRows) {
-      let title = await row.findElement(By.className("title")).getText();
-      let author = await row.findElement(By.className("author")).getText();
-      let avgRating = await row.findElement(By.className("avg_rating")).getText();
-      let cover = await row.findElement(By.css("img")).getAttribute("src");
-      cover = cover.replace("._SY75_","");
-      let url = await row.findElement(By.css("a")).getAttribute("href");
-      books.push({title, author, avgRating, cover, url});
-    }
-    return books;
+  let ratedRows = await driver.findElements(By.className("bookalike"));
+  ratedRows.length = 5;
+  let books = [];
+  for (let row of ratedRows) {
+    let title = await row.findElement(By.className("title")).getText();
+    let author = await row.findElement(By.className("author")).getText();
+    let avgRating = await row.findElement(By.className("avg_rating")).getText();
+    let cover = await row.findElement(By.css("img")).getAttribute("src");
+    cover = cover.replace("._SY75_", "");
+    let url = await row.findElement(By.css("a")).getAttribute("href");
+    books.push({ title, author, avgRating, cover, url });
+  }
+  return books;
 }
 
-async function getPopularMonth () {
+async function getPopularMonth() {
   let today = new Date()
   let mm = today.getMonth() + 1;
   let yyyy = today.getFullYear();
   driver.get(`${baseUrl}/book/popular_by_date/${yyyy}/${mm}`);
   let popularBooks = await driver.findElements(By.className("BookListItem"));
   let books = [];
-  for (let book of popularBooks){ 
+  for (let book of popularBooks) {
     let title = await book.findElement(By.className("Text__title3")).getText();
     let author = await book.findElement(By.className("BookListItem__authors")).getText();
     let avgRating = await book.findElement(By.className("AverageRating__ratingValue")).getText();
     let cover = await book.findElement(By.className("ResponsiveImage")).getAttribute("src");
     let url = await book.findElement(By.className("BookCard__clickCardTarget")).getAttribute("href");
-    books.push({title, author, avgRating, cover, url});
+    books.push({ title, author, avgRating, cover, url });
   }
   return books;
 
 }
 
-async function addUser (discordID, goodreadsID) {
+async function addUser(discordID, goodreadsID) {
   let results = await database.searchUser(discordID);
-  if (results == null){
+  if (results == null) {
     await database.storeUser(discordID, goodreadsID);
     return true;
-  } else {return false;}
+  } else { return false; }
 }
 
-function parseGoodreadsUserID(userID){
-  if (userID.includes("goodreads.com")){
+function parseGoodreadsUserID(userID) {
+  if (userID.includes("goodreads.com")) {
     let splitUser = userID.split("/");
-    if (splitUser[splitUser.length -1] == ""){
-      return splitUser[splitUser.length -2];
+    if (splitUser[splitUser.length - 1] == "") {
+      return splitUser[splitUser.length - 2];
     } else {
-      return splitUser[splitUser.length -1]
+      return splitUser[splitUser.length - 1]
     }
   } else {
     return userID;
@@ -110,8 +110,15 @@ function parseGoodreadsUserID(userID){
 
 };
 
+async function validifyUserID(userID) {
+  driver.get(`${baseUrl}/user/show/${userID}`);
+  let mainContent = await driver.findElement(By.css(".leftContainer  .gr-button")).getText();
+  console.log(mainContent);
+  return !mainContent.includes("Back to the Goodreads homepage")
+}
+
 const { REST, Routes, Message, MessageComponentInteraction } = require('discord.js');
-const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);  
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
@@ -146,55 +153,63 @@ client.on('interactionCreate', async interaction => {
   if (interaction.commandName === 'search') {
     await interaction.deferReply();
     let searchResults = await searchBook(interaction.options.getString("title"));
-    let embed = embedUtils.createEmbed(searchResults[0], {name:"Author", value:searchResults[0].author, inline:true});
-    await interaction.editReply({embeds : [embed]});
+    let embed = embedUtils.createEmbed(searchResults[0], { name: "Author", value: searchResults[0].author, inline: true });
+    await interaction.editReply({ embeds: [embed] });
   }
 
   if (interaction.commandName === 'currently_reading') {
     await interaction.deferReply();
     let userID = "";
-    if (interaction.options.getString("user") != null){
+    if (interaction.options.getString("user") != null) {
       userID = parseGoodreadsUserID(interaction.options.getString("user"));
     } else {
       let searchResult = await database.searchUser(interaction.user.id);
       userID = searchResult['goodreadsID']
     }
-    let currentResults = await getCurrentReading(userID);
-    let embeds = [];
-    for (let i = 0; i < currentResults.length; i++) {
+    if (await validifyUserID(userID)) {
+      let currentResults = await getCurrentReading(userID);
+      let embeds = [];
+      for (let i = 0; i < currentResults.length; i++) {
         let embed = embedUtils.createEmbed(currentResults[i], ...embedUtils.commonFields(currentResults[i]))
         embeds.push(embed);
+      }
+      await interaction.editReply({ embeds });
+    } else {
+      await interaction.editReply("Goodreads ID invalid");
     }
-    await interaction.editReply({embeds});
   }
 
   if (interaction.commandName === 'top_rated') {
     await interaction.deferReply();
     let userID = "";
-    if (interaction.options.getString("user") != null){
+    if (interaction.options.getString("user") != null) {
       userID = parseGoodreadsUserID(interaction.options.getString("user"));
     } else {
       let searchResult = await database.searchUser(interaction.user.id);
       userID = searchResult['goodreadsID']
     }
-    let topRated = await getTopRated(userID);
-    let embeds = [];
-    for (let i = 0; i < topRated.length; i++) {
-      let embed = embedUtils.createEmbed(topRated[i], ...embedUtils.commonFields(topRated[i]));
-      embeds.push(embed);
+    if (await validifyUserID(userID)) {
+      let topRated = await getTopRated(userID);
+      let embeds = [];
+      for (let i = 0; i < topRated.length; i++) {
+        let embed = embedUtils.createEmbed(topRated[i], ...embedUtils.commonFields(topRated[i]));
+        embeds.push(embed);
+      }
+      await interaction.editReply({ embeds });
+    } else {
+      await interaction.editReply("Goodreads ID invalid");
     }
-    await interaction.editReply({embeds});
   }
 
   if (interaction.commandName === 'popular_month') {
     await interaction.deferReply();
     let popularBooks = await getPopularMonth();
     let embeds = [];
-    for (let i = 0; i < 5; i++){
+    for (let i = 0; i < 5; i++) {
       let embed = embedUtils.createEmbed(popularBooks[i], ...embedUtils.commonFields(popularBooks[i]))
-      embeds.push(embed);  
+      embeds.push(embed);
     }
-    interaction.editReply({embeds});
+    interaction.editReply({ embeds });
   }
 
   if (interaction.commandName === 'add_user') {
@@ -204,7 +219,7 @@ client.on('interactionCreate', async interaction => {
       interaction.reply("User added to database")
     } else {
       interaction.reply("User not added to database :(")
-    } 
+    }
   }
 
 });
